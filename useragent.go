ed2353abcedf8e58e0fe0ee06181aa85bbad4e66
Edward Middleton <edward.middleton@vortorus.net>
2013-08-02 23:08:10 +0900

    initial import

---
+++ useragent.go
@@ -0,0 +1,456 @@
+package useragent
+
+import (
+	"flag"
+	"log"
+	"fmt"
+	"strings"
+	"regexp"
+	"bitbucket.org/zombiezen/goray/yaml/data"
+	"bitbucket.org/zombiezen/goray/yaml/parser"
+	"os"
+)
+
+var (
+	file = flag.String("file", "./useragent.yml", "Useragent data")
+	application_types        = map[string]ApplicationType{}
+	device_types             = map[string]DeviceType{}
+	rendering_engines        = map[string]RenderingEngine{}
+	manufacturers            = map[string]Manufacturer{}
+	applications             = map[string]Application{}
+	browser_types            = map[string]BrowserType{}
+	browsers                 = []Browser{}
+	browser_unknown          *Browser
+	operating_systems        = []OperatingSystem{}
+	operating_system_unknown *OperatingSystem
+)
+
+type ApplicationType struct {
+	Key             string
+	Name            string
+}
+
+type DeviceType struct {
+	Key             string
+	Name            string
+}
+
+type BrowserType struct {
+	Key             string
+	Name            string
+}
+
+type RenderingEngine struct {
+	Key             string
+	Name            string
+}
+
+type Manufacturer struct {
+	Id              uint16
+	Key             string
+	Name            string
+}
+
+type Application struct {
+	Id	        uint16
+	Key             string
+	Name	        string
+	aliases         []string
+	applicationType	string
+	manufacturer    string
+}
+
+type Browser struct {
+	Id              uint16
+	Key             string
+	Name            string
+	aliases	        []string
+	excludeList     []string
+	browserType     string
+	manufacturer    string
+	renderingEngine string
+	parent          *Browser
+	children        []Browser
+	versionRegexp   *regexp.Regexp
+}
+
+type OperatingSystem struct {
+	Id	        uint16
+	Key             string
+	Name	        string
+	aliases	        []string
+	excludeList     []string
+	manufacturer    string
+	deviceType      string
+	parent          *OperatingSystem
+	children        []OperatingSystem
+}
+
+func (b Browser)BrowserType()(bt BrowserType){
+	if b.browserType == "" && b.parent != nil{
+		return b.parent.BrowserType()
+	} else {
+		return browser_types[b.browserType]
+	}
+}
+
+func (b Browser)Manufacturer()(m Manufacturer){
+	if b.manufacturer == "" && b.parent != nil {
+		return b.parent.Manufacturer()
+	} else {
+		return manufacturers[b.manufacturer]
+	}
+}
+
+func (b Browser)RenderingEngine()(re RenderingEngine){
+	if b.renderingEngine == "" && b.parent != nil {
+		return b.parent.RenderingEngine()
+	} else {
+		return rendering_engines[b.renderingEngine]
+	}
+}
+
+func (b Browser)getVersionRegexp()(regex *regexp.Regexp){
+	if b.versionRegexp == nil && b.parent != nil {
+		return b.parent.getVersionRegexp()
+	} else {
+		return b.versionRegexp
+	}
+}
+
+func (b Browser)Version(ua string)(v []string){
+	regexp := b.getVersionRegexp()
+//	fmt.Printf("%#v\n",regexp)
+	if regexp != nil {
+//		fmt.Printf("%#v\n",regexp)
+		rslt := regexp.FindStringSubmatch(ua)
+		if rslt != nil {
+			if len(rslt) < 4 {
+				rslt = append(rslt,"0")
+			}
+			return rslt
+		}
+	}
+	return v
+}
+
+func matchesList(str string, ms []string)(ok bool){
+	for _,m := range ms {
+		rslt := strings.Index(strings.ToLower(str),strings.ToLower(m))
+		if rslt != -1 {
+			return true
+		}
+	}
+	return false
+}
+
+func (b Browser)matched(ua string)(r Browser, ok bool){
+	if matchesList(ua,b.aliases) {
+		if len(b.children) > 0 {
+			for _,child := range b.children {
+//				fmt.Printf("child match: %#v\n",child.Key)
+				matchedChild,childMatched := child.matched(ua)
+				if childMatched {
+					return matchedChild,true
+				}
+			}
+		}
+		if matchesList(ua,b.excludeList) == false {
+			return b,true
+		}
+	}
+	return r,false
+}
+
+func FindBrowser(ua string)(b Browser) {
+	for _,browser := range browsers {
+//		fmt.Printf("parent match: %#v\n",browser.Key)
+		match,browserMatched := browser.matched(ua)
+		if browserMatched {
+			return match
+		}
+	}
+	return *browser_unknown
+}
+
+func initBrowser(parent *Browser, browser_key string, browser_node *parser.Mapping)(b Browser, err error) {
+	var browser_id uint16
+	b.Key = browser_key
+	b.parent = parent
+	for _,browser_pair := range browser_node.Pairs {
+		browser_key,_ := browser_pair.Key.Data().(string)
+		browser_value := browser_pair.Value
+		switch browser_key {
+		case "id":
+			browser_id = uint16(browser_value.Data().(int64))
+		case "name":
+			b.Name = browser_value.Data().(string)
+		case "aliases","exclude_list":
+			seq,_ := browser_value.(*parser.Sequence)
+			matches := []string{}
+			for _,match := range seq.Nodes {
+				if match.Data() != nil {
+					matches = append(matches,match.Data().(string))
+				}
+			}
+			if browser_key == "aliases" {
+				b.aliases = matches
+			} else {
+				b.excludeList = matches
+			}
+		case "browser_type":
+			b.browserType     = browser_value.Data().(string)
+		case "manufacturer":
+			b.manufacturer    = browser_value.Data().(string)
+		case "rendering_engine":
+			b.renderingEngine = browser_value.Data().(string)
+		case "children":
+			for _, child_pair := range browser_value.(*parser.Mapping).Pairs {
+				child_key            := child_pair.Key.Data().(string)
+				child_node           := child_pair.Value.(*parser.Mapping)
+				child_browser,_      := initBrowser(&b,child_key,child_node)
+				b.children = append(b.children,child_browser)
+			}
+		case "version_regex":
+			b.versionRegexp   = regexp.MustCompile(browser_value.Data().(string))
+		case "parent":
+			panic("parent is no longer supported, nest children instead")
+		}
+	}
+	b.Id = b.Manufacturer().Id << 8 + browser_id
+	return b,nil
+}
+
+func (os OperatingSystem)Manufacturer()(m Manufacturer){
+	if os.manufacturer == "" && os.parent != nil {
+		return os.parent.Manufacturer()
+	} else {
+		return manufacturers[os.manufacturer]
+	}
+}
+
+func (os OperatingSystem)DeviceType()(dt DeviceType){
+	if os.deviceType == "" && os.parent != nil {
+		return os.parent.DeviceType()
+	} else {
+		return device_types[os.deviceType]
+	}
+}
+
+func (os OperatingSystem)matched(ua string)(r OperatingSystem, ok bool){
+	if matchesList(ua,os.aliases) {
+		if len(os.children) > 0 {
+			for _,child := range os.children {
+//				fmt.Printf("child match: %#v\n",child.Key)
+				matchedChild,childMatched := child.matched(ua)
+				if childMatched {
+					return matchedChild,true
+				}
+			}
+		}
+		if matchesList(ua,os.excludeList) == false {
+			return os,true
+		}
+	}
+	return r,false
+}
+
+func FindOperatingSystem(ua string)(os OperatingSystem) {
+	for _,os := range operating_systems {
+//		fmt.Printf("parent match: %#v\n",os.Key)
+		match,osMatched := os.matched(ua)
+		if osMatched {
+			return match
+		}
+	}
+	return *operating_system_unknown
+}
+
+func initOperatingSystem(parent *OperatingSystem, operating_system_key string, operating_system_node *parser.Mapping)(os OperatingSystem, err error) {
+	var operating_system_id uint16
+	os.Key = operating_system_key
+	os.parent = parent
+	for _,operating_system_pair := range operating_system_node.Pairs {
+		operating_system_key,_ := operating_system_pair.Key.Data().(string)
+		operating_system_value := operating_system_pair.Value
+		switch operating_system_key {
+		case "id":
+			operating_system_id = uint16(operating_system_value.Data().(int64))
+		case "name":
+			os.Name = operating_system_value.Data().(string)
+		case "aliases","exclude_list":
+			seq,_ := operating_system_value.(*parser.Sequence)
+			matches := []string{}
+			for _,match := range seq.Nodes {
+				if match.Data() != nil {
+					matches = append(matches,match.Data().(string))
+				}
+			}
+			if operating_system_key == "aliases" {
+				os.aliases = matches
+			} else {
+				os.excludeList = matches
+			}
+		case "manufacturer":
+			os.manufacturer    = operating_system_value.Data().(string)
+		case "device_type":
+			os.deviceType      = operating_system_value.Data().(string)
+		case "children":
+			for _, child_pair := range operating_system_value.(*parser.Mapping).Pairs {
+				child_key  := child_pair.Key.Data().(string)
+				child_node := child_pair.Value.(*parser.Mapping)
+				child_os,_ := initOperatingSystem(&os,child_key,child_node)
+				os.children = append(os.children,child_os)
+			}
+		default:
+			panic("unknown tag in operating system")
+		}
+	}
+	os.Id = os.Manufacturer().Id << 8 + operating_system_id
+	return os,nil
+}
+
+func init() {
+	fileReader,err := os.Open(*file)
+	if err != nil {
+		log.Fatalf("os.Open(%q): %s", *file, err)
+	}
+	yamlParser := parser.New(fileReader,data.CoreSchema,data.DefaultConstructor,nil)
+	doc, err := yamlParser.ParseDocument()
+	if err != nil {
+		log.Fatal(err)
+	}
+	m, ok := doc.Content.(*parser.Mapping)
+	if !ok {
+		panic("Top level Yaml is not a map")
+	}
+	for _,pair := range m.Pairs {
+		key_name, ok := pair.Key.Data().(string)
+		if !ok {
+			panic("Complex type for a map key?")
+		}
+		key_node, ok := pair.Value.(*parser.Mapping)
+		if !ok {
+			panic(fmt.Sprintf("Should be map but was %#v",pair.Value))
+		}
+
+		switch key_name {
+		case "application_types":
+			for _,at_pair := range key_node.Pairs {
+				at := ApplicationType{}
+				at.Key = at_pair.Key.Data().(string)
+				at.Name = at_pair.Value.Data().(string)
+				application_types[":"+at.Key] = at
+			}
+		case "manufacturers":
+			for _,m_pairs := range key_node.Pairs {
+				m := Manufacturer{}
+				m.Key = m_pairs.Key.Data().(string)
+				m_map := m_pairs.Value.(*parser.Mapping)
+				for _,m_pair := range m_map.Pairs {
+					key   := m_pair.Key.Data().(string)
+					value := m_pair.Value.Data()
+					switch key {
+					case "id":
+						m.Id = uint16(value.(int64))
+					case "name":
+						m.Name = value.(string)
+					default:
+						panic("invalid manufacturer key")
+					}
+				}
+				manufacturers[":"+m.Key] = m
+//				fmt.Printf("%#v -> %#v\n",m_key,m)
+			}
+		case "applications":
+			for _,a_pairs := range key_node.Pairs {
+				a := Application{}
+				a.Key = a_pairs.Key.Data().(string)
+				a_map := a_pairs.Value.(*parser.Mapping)
+				for _,a_pair := range a_map.Pairs {
+					key   := a_pair.Key.Data().(string)
+					value := a_pair.Value
+					switch key {
+					case "id":
+						a.Id = uint16(value.Data().(int64))
+					case "name":
+						a.Name = value.Data().(string)
+					case "aliases":
+						seq,_ := value.(*parser.Sequence)
+						matches := []string{}
+						for _,match := range seq.Nodes {
+							if match.Data() != nil {
+								matches = append(matches,match.Data().(string))
+							}
+						}
+						a.aliases = matches
+					case "application_type":
+						a.applicationType = value.Data().(string)
+					case "manufacturer":
+						a.manufacturer = value.Data().(string)
+					default:
+						panic("invalid device type key")
+					}
+				}
+				applications[":"+a.Key] = a
+//				fmt.Printf("%#v\n",a)
+			}
+		case "device_types":
+			for _,dt_pair := range key_node.Pairs {
+				dt := DeviceType{}
+				dt.Key = dt_pair.Key.Data().(string)
+				dt.Name = dt_pair.Value.Data().(string)
+				device_types[":"+dt.Key] = dt
+//				fmt.Printf("%#v\n",dt)
+			}
+		case "browser_types":
+			for _,bt_pair := range key_node.Pairs {
+				bt := BrowserType{}
+				bt.Key = bt_pair.Key.Data().(string)
+				bt.Name = bt_pair.Value.Data().(string)
+				browser_types[":"+bt.Key] = bt
+//				fmt.Printf("%#v\n",bt)
+			}
+		case "rendering_engines":
+			for _,re_pair := range key_node.Pairs {
+				re := RenderingEngine{}
+				re.Key = re_pair.Key.Data().(string)
+				re.Name = re_pair.Value.Data().(string)
+				rendering_engines[":"+re.Key] = re
+//				fmt.Printf("%#v\n",re)
+			}
+		case "browsers":
+			for _,browser_pair := range key_node.Pairs {
+				browser_key,_ := browser_pair.Key.Data().(string)
+				browser_node,_ := browser_pair.Value.(*parser.Mapping)
+//				fmt.Printf("Browser{%#v}\n",browser_key)
+				browser,err := initBrowser(nil,browser_key,browser_node)
+				if err != nil {
+					panic(fmt.Sprintf("error: initOperatingSystem -> %#v",err))
+				}
+				browsers = append(browsers,browser)
+				if browser_key == "unknown" {
+					browser_unknown = &browser
+				}
+//				fmt.Printf("%#v\n",browser)
+			}
+
+		case "operating_systems":
+			for _,os_pair := range key_node.Pairs {
+				os_key,_ := os_pair.Key.Data().(string)
+				os_node,_ := os_pair.Value.(*parser.Mapping)
+//				fmt.Printf("OperatingSystem{%#v}\n",os_key)
+				os,err := initOperatingSystem(nil,os_key,os_node)
+				if err != nil {
+					panic(fmt.Sprintf("error: initOperatingSystem -> %#v",err))
+				}
+				operating_systems = append(operating_systems,os)
+				if os_key == "unknown" {
+					operating_system_unknown = &os
+				}
+//				fmt.Printf("%#v\n",os)
+			}
+		}
+
+	}
+}
---
+++ useragent_test.go
@@ -0,0 +1,211 @@
+package useragent
+
+import (
+	"fmt"
+	"testing"
+	"bitbucket.org/zombiezen/goray/yaml/data"
+	"bitbucket.org/zombiezen/goray/yaml/parser"
+	"log"
+	"os"
+)
+
+var (
+	browser_parse                = []BrowserParse{}
+	browser_version              = []BrowserVersion{}
+	browser_incomplete           = []BrowserIncomplete{}
+	operating_system_device_type = []OperatingSystemDeviceType{}
+	operating_system_parse       = []OperatingSystemParse{}
+)
+
+type BrowserParse struct {
+	Key        string
+	UserAgents []string
+}
+
+type Version struct {
+	Full  string
+	Major string
+	Minor string
+}
+
+type BrowserVersion struct {
+	UserAgent  string
+	Version    *Version
+}
+
+type BrowserIncomplete string
+
+type OperatingSystemDeviceType struct {
+	DeviceType string
+	UserAgents []string
+}
+
+type OperatingSystemParse struct {
+	Key        string
+	UserAgents []string
+}
+
+func init() {
+	fileReader,err := os.Open("useragent_test.yml")
+	if err != nil {
+		log.Fatalf("os.Open(%q): %s", *file, err)
+	}
+	yamlParser := parser.New(fileReader,data.CoreSchema,data.DefaultConstructor,nil)
+	doc, err := yamlParser.ParseDocument()
+	if err != nil {
+		log.Fatal(err)
+	}
+	m, ok := doc.Content.(*parser.Mapping)
+	if !ok {
+		panic("Top level Yaml is not a map")
+	}
+	for _,pair := range m.Pairs {
+		key_name, ok := pair.Key.Data().(string)
+		if !ok {
+			panic("Complex type for a map key?")
+		}
+		key_node, ok := pair.Value.(*parser.Mapping)
+		if !ok {
+			panic(fmt.Sprintf("Should be map but was %#v",pair.Value))
+		}
+		switch key_name {
+		case "browser":
+			for _,section_pairs := range key_node.Pairs {
+				section_key := section_pairs.Key.Data().(string)
+				section_value := section_pairs.Value
+				switch section_key {
+				case "parse":
+					for _,test_pairs := range section_value.(*parser.Mapping).Pairs {
+						bp := BrowserParse{}
+						bp.Key = test_pairs.Key.Data().(string)
+						uas := []string{}
+						seq,_ := test_pairs.Value.(*parser.Sequence)
+						for _,ua := range seq.Nodes {
+							uas = append(uas,ua.Data().(string))
+						}
+						bp.UserAgents = uas
+						browser_parse = append(browser_parse,bp)
+					}
+				case "version":
+					seq,_ := section_value.(*parser.Sequence)
+					for _,pv := range seq.Nodes {
+						bv := BrowserVersion{}
+						for _,key_pairs := range pv.(*parser.Mapping).Pairs {
+							switch key_pairs.Key.Data().(string) {
+							case "user_agent":
+								bv.UserAgent = key_pairs.Value.Data().(string)
+							case "version":
+								v := Version{}
+								for _,v_pair := range key_pairs.Value.(*parser.Mapping).Pairs {
+									switch v_pair.Key.Data().(string) {
+									case "full":
+										// fmt.Printf("%#v\n",v_pair.Value.(*parser.Scalar).String())
+										v.Full = v_pair.Value.(*parser.Scalar).String()
+									case "major":
+										// fmt.Printf("%#v\n",v_pair.Value.(*parser.Scalar).String())
+										v.Major = v_pair.Value.(*parser.Scalar).String()
+									case "minor":
+										// fmt.Printf("%#v\n",v_pair.Value.(*parser.Scalar).String())
+										v.Minor = v_pair.Value.(*parser.Scalar).String()
+									}
+								}
+								bv.Version = &v
+							}
+						}
+						browser_version = append(browser_version,bv)
+					}
+				case "incomplete":
+				}
+			}
+		case "operating_system":
+			for _,section_pairs := range key_node.Pairs {
+				section_key := section_pairs.Key.Data().(string)
+				section_value := section_pairs.Value
+				switch section_key {
+				case "device_type":
+					for _,dt_pair := range section_value.(*parser.Mapping).Pairs {
+						dt := OperatingSystemDeviceType{}
+						dt.DeviceType = dt_pair.Key.Data().(string)
+						uas := []string{}
+						seq,_ := dt_pair.Value.(*parser.Sequence)
+						for _,ua := range seq.Nodes {
+							uas = append(uas,ua.Data().(string))
+						}
+						dt.UserAgents = uas
+						operating_system_device_type = append(operating_system_device_type,dt)
+					}
+				case "parse":
+					for _,test_pairs := range section_value.(*parser.Mapping).Pairs {
+						osp := OperatingSystemParse{}
+						osp.Key = test_pairs.Key.Data().(string)
+						uas := []string{}
+						seq,_ := test_pairs.Value.(*parser.Sequence)
+						for _,ua := range seq.Nodes {
+							uas = append(uas,ua.Data().(string))
+						}
+						osp.UserAgents = uas
+						operating_system_parse = append(operating_system_parse,osp)
+					}
+				}
+			}
+		}
+	}
+}
+
+func TestBrowserIdentification(t *testing.T) {
+	for _,bp := range browser_parse {
+		for _,ua := range bp.UserAgents {
+			b := FindBrowser(ua)
+			if bp.Key != b.Key {
+				t.Errorf("expected: %#v\ngot: %#v\n",bp.Key,b.Key)
+			}
+			// fmt.Printf("%#v\n",b.Key)
+		}
+	}
+}
+
+func TestBrowserVersionParsing(t *testing.T) {
+	for _,bv := range browser_version {
+		b := FindBrowser(bv.UserAgent)
+		// fmt.Printf("%#v\n",b.Name)
+		v := b.Version(bv.UserAgent)
+		// fmt.Printf("%#v\n",v)
+		if bv.Version == nil {
+			if v != nil {
+				t.Errorf("Version\nexpected: %#v\ngot: %#v\n",bv.Version,v)
+			}
+		} else {
+			if v[1] != bv.Version.Full {
+				t.Errorf("Version.Full\nexpected: %#v\ngot: %#v\n",bv.Version.Full,v[1])
+			}
+			if v[2] != bv.Version.Major {
+				t.Errorf("Version.Major\nexpected: %#v\ngot: %#v\n",bv.Version.Major,v[2])
+			}
+			if v[3] != bv.Version.Minor {
+				t.Errorf("Version.Minor\nexpected: %#v\ngot: %#v\n",bv.Version.Minor,v[3])
+			}
+		}
+	}
+}
+
+func TestOperatingSystemDeviceTypeIdentification(t *testing.T) {
+	for _,osdt := range operating_system_device_type {
+		for _,ua := range osdt.UserAgents {
+			os := FindOperatingSystem(ua)
+			if osdt.DeviceType != os.DeviceType().Key {
+				t.Errorf("OperatingSystem.DeviceType[%#v]\nexpected: %#v\ngot: %#v\n",osdt.DeviceType,osdt.DeviceType,os.DeviceType().Key)
+			}
+		}
+	}
+}
+
+func TestOperatingSystemIdentification(t *testing.T) {
+	for _,osp := range operating_system_parse {
+		for _,ua := range osp.UserAgents {
+			os := FindOperatingSystem(ua)
+			if osp.Key != os.Key {
+				t.Errorf("OperatingSystem\nexpected: %#v\ngot: %#v\n",osp.Key,os.Key)
+			}
+		}
+	}
+}
